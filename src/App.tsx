539f1b539d48b01b48aa6576a58e9e56c4a97154
y-minion <dudals2963@tukorea.ac.kr>
2025-10-26 15:34:06 +0900

    shadcn 라이브러리 설치
    라이브러리 설치 및 환경 구성 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
+import { cn } from "./lib/utils";
 
 function App() {
-  return <div className="text-2xl font-bold underline">Hello world</div>;
+  const isActive = false;
+  return (
+    <div>
+      <div className={cn(isActive ? "text-green-400" : "text-red-600")}>
+        isActive
+      </div>
+      <Button>버튼!</Button>
+      <div className="text-primary">Primary</div>
+      <div className="text-muted">Muted</div>
+      <div className="text-destructive">Destructive</div>
+    </div>
+  );
 }
 
 export default App;
