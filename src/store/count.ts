7bd72d9fe0c58b360a44ddef237fbe5760add562
y-minion <dudals2963@tukorea.ac.kr>
2025-10-27 16:37:47 +0900

    devtools 미들웨어 도입으로 스토어의 상태 디버깅을 쉽게 할 수 있도록 함

--- src/store/count.ts
+++ src/store/count.ts
@@ -4,36 +4,42 @@ import {
   subscribeWithSelector,
   persist,
   createJSONStorage,
+  devtools,
 } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
 const useCountStore = create(
-  persist(
-    subscribeWithSelector(
-      immer(
-        combine({ count: 0 }, (set, get) => ({
-          actions: {
-            increase() {
-              set((state) => {
-                state.count += 1;
-              }); //set함수는 스토어의 상태값을 변화시키는데 이때, 전체 스토어를 넘길 필요 없이 변화하고 싶은 상태만 골라서 반환하면 된다. 드래서 당연히 이때의 반환값은 객체다.
+  devtools(
+    persist(
+      subscribeWithSelector(
+        immer(
+          combine({ count: 0 }, (set, get) => ({
+            actions: {
+              increase() {
+                set((state) => {
+                  state.count += 1;
+                }); //set함수는 스토어의 상태값을 변화시키는데 이때, 전체 스토어를 넘길 필요 없이 변화하고 싶은 상태만 골라서 반환하면 된다. 드래서 당연히 이때의 반환값은 객체다.
+              },
+              decrease() {
+                set((state) => {
+                  state.count -= 1;
+                });
+              },
             },
-            decrease() {
-              set((state) => {
-                state.count -= 1;
-              });
-            },
-          },
-        })),
+          })),
+        ),
       ),
+      {
+        name: "countStore",
+        partialize: (store) => ({
+          //스토어 전체를 로컬 스토리지에 저장하는게 아니라 선택적으로 설정해서 저장할 수 있다.
+          count: store.count, //count 상태만 로컬스토리지에 저장해서 사용하겠다.
+        }),
+        storage: createJSONStorage(() => sessionStorage),
+      },
     ),
     {
       name: "countStore",
-      partialize: (store) => ({
-        //스토어 전체를 로컬 스토리지에 저장하는게 아니라 선택적으로 설정해서 저장할 수 있다.
-        count: store.count, //count 상태만 로컬스토리지에 저장해서 사용하겠다.
-      }),
-      storage: createJSONStorage(() => sessionStorage),
     },
   ),
 );
