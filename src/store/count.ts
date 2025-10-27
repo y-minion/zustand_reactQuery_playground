4e1c4a43be7c3e739a3831c9e76c6e44c42e7b98
y-minion <dudals2963@tukorea.ac.kr>
2025-10-27 16:30:42 +0900

    persist미들웨어를 통해 count 상태를 세션에 저장

--- src/store/count.ts
+++ src/store/count.ts
@@ -1,25 +1,40 @@
 import { create } from "zustand";
-import { combine, subscribeWithSelector } from "zustand/middleware";
+import {
+  combine,
+  subscribeWithSelector,
+  persist,
+  createJSONStorage,
+} from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
 const useCountStore = create(
-  subscribeWithSelector(
-    immer(
-      combine({ count: 0 }, (set, get) => ({
-        actions: {
-          increase() {
-            set((state) => {
-              state.count += 1;
-            }); //set함수는 스토어의 상태값을 변화시키는데 이때, 전체 스토어를 넘길 필요 없이 변화하고 싶은 상태만 골라서 반환하면 된다. 드래서 당연히 이때의 반환값은 객체다.
+  persist(
+    subscribeWithSelector(
+      immer(
+        combine({ count: 0 }, (set, get) => ({
+          actions: {
+            increase() {
+              set((state) => {
+                state.count += 1;
+              }); //set함수는 스토어의 상태값을 변화시키는데 이때, 전체 스토어를 넘길 필요 없이 변화하고 싶은 상태만 골라서 반환하면 된다. 드래서 당연히 이때의 반환값은 객체다.
+            },
+            decrease() {
+              set((state) => {
+                state.count -= 1;
+              });
+            },
           },
-          decrease() {
-            set((state) => {
-              state.count -= 1;
-            });
-          },
-        },
-      })),
+        })),
+      ),
     ),
+    {
+      name: "countStore",
+      partialize: (store) => ({
+        //스토어 전체를 로컬 스토리지에 저장하는게 아니라 선택적으로 설정해서 저장할 수 있다.
+        count: store.count, //count 상태만 로컬스토리지에 저장해서 사용하겠다.
+      }),
+      storage: createJSONStorage(() => sessionStorage),
+    },
   ),
 );
 
@@ -28,7 +43,7 @@ useCountStore.subscribe(
   (count, prevCount) => {
     //listener
     console.log(count, prevCount);
-    const store = useCountStore.getState();
+    const store = useCountStore.getState(); // 해당하는 스토어의 값을 구독이 아니라 단순히 스토어의 현재상태를 한번 가져온다.
   },
 );
 
