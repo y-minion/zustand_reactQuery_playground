ba333426ad6f7ff14b689673f225d9c676ae49d2
y-minion <dudals2963@tukorea.ac.kr>
2025-10-27 12:45:31 +0900

    ♻️ count스토어 셀렉터 문법 도입
    불필요한 리렌더링을 방지하기 위해 필요한 상태만 구독하는 셀렉터 문법으로 수정 완료. 이때 여래 상태를 구독할때 한번에 구독할 수 있게 하기 위해 store객체의 increase와decrease를 actions로 묶었음.

--- src/components/counter/controller.tsx
+++ src/components/counter/controller.tsx
@@ -2,7 +2,7 @@ import { useCountStore } from "@/store/count";
 import { Button } from "../ui/button";
 
 export default function Controller() {
-  const { increase, decrease } = useCountStore();
+  const { increase, decrease } = useCountStore((store) => store.actions);
   return (
     <div>
       <Button onClick={() => decrease()}>-</Button>
