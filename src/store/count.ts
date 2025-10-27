abed0488ddfb569f693c0de18b61b91d1ae5f40a
y-minion <dudals2963@tukorea.ac.kr>
2025-10-27 14:54:34 +0900

    ♻️ Immer 미들웨어 도입

--- src/store/count.ts
+++ src/store/count.ts
@@ -1,24 +1,25 @@
 import { create } from "zustand";
+import { combine } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 
-type Store = {
-  count: number;
-  actions: {
-    increase: () => void;
-    decrease: () => void;
-  };
-};
-
-export const useCountStore = create<Store>((set) => ({
-  count: 0,
-  actions: {
-    increase() {
-      set((store) => ({ count: store.count + 1 })); //set함수는 스토어의 상태값을 변화시키는데 이때, 전체 스토어를 넘길 필요 없이 변화하고 싶은 상태만 골라서 반환하면 된다. 드래서 당연히 이때의 반환값은 객체다.
-    },
-    decrease() {
-      set((store) => ({ count: store.count - 1 }));
-    },
-  },
-}));
+const useCountStore = create(
+  immer(
+    combine({ count: 0 }, (set, get) => ({
+      actions: {
+        increase() {
+          set((state) => {
+            state.count += 1;
+          }); //set함수는 스토어의 상태값을 변화시키는데 이때, 전체 스토어를 넘길 필요 없이 변화하고 싶은 상태만 골라서 반환하면 된다. 드래서 당연히 이때의 반환값은 객체다.
+        },
+        decrease() {
+          set((state) => {
+            state.count -= 1;
+          });
+        },
+      },
+    })),
+  ),
+);
 
 export const useCount = () => {
   const count = useCountStore((store) => store.count);
