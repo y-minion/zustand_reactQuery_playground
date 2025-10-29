d960bd641a25c63c65d0bb29e634b6bc657c229f
y-minion <dudals2963@tukorea.ac.kr>
2025-10-29 12:35:21 +0900

    Todo 타입 수정

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,11 @@
-export type Todo = { id: number; content: string };
+export type Todo = { id: string; content: string };
 export type TodoList = Todo[];
 export type TodoState = {
   todoList: TodoList;
 };
 export type TodoActions = {
   addTodo: (content: string) => void;
-  deleteTodo: (id: number) => void;
+  deleteTodo: (id: string) => void;
 };
 
 export type TodoStore = TodoState & { actions: TodoActions };
