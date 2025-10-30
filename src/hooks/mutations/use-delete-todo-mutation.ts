a591f1b2a025f9bdebe3e16715f7c24230cd84f3
y-minion <dudals2963@tukorea.ac.kr>
2025-10-30 19:51:23 +0900

    todo데이터 수정,삭제 로직 수정 완료

--- src/hooks/mutations/use-delete-todo-mutation.ts
+++ src/hooks/mutations/use-delete-todo-mutation.ts
@@ -1,6 +1,6 @@
 import { deleteTodo } from "@/api/delete-todo";
 import { QUERY_KEYS } from "@/lib/constants";
-import type { TodoList } from "@/types";
+import type { Todo, TodoList } from "@/types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useDeleteTodoMutation() {
@@ -9,10 +9,15 @@ export function useDeleteTodoMutation() {
     mutationFn: deleteTodo,
 
     onSuccess: (deletedTodo) => {
-      queryClient.setQueryData<TodoList>(QUERY_KEYS.todo.list, (prevData) => {
+      //총 2곳의 캐시 데이터를 수정해 줘야한다.
+      queryClient.removeQueries({
+        queryKey: QUERY_KEYS.todo.detail(deletedTodo.id),
+      }); // 디테일 toDo 캐시 데이터 수정
+
+      queryClient.setQueryData<string[]>(QUERY_KEYS.todo.list, (prevData) => {
         if (!prevData) return [];
-        return prevData?.filter((todo) => todo.id !== deletedTodo.id);
-      });
+        return prevData.filter((id) => id !== deletedTodo.id);
+      }); // todo Id들이 모여있는 배열 캐시 수정
     },
   });
 }
--- src/hooks/mutations/use-update-todo-mutation.ts
+++ src/hooks/mutations/use-update-todo-mutation.ts
@@ -1,6 +1,6 @@
 import { updateTodo } from "@/api/update-todo";
 import { QUERY_KEYS } from "@/lib/constants";
-import type { TodoList } from "@/types";
+import type { Todo, TodoList } from "@/types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useUpdateTodoMutation() {
@@ -9,35 +9,33 @@ export function useUpdateTodoMutation() {
     mutationFn: updateTodo,
 
     onMutate: async (updatedTodo) => {
-      await queryClient.cancelQueries({ queryKey: QUERY_KEYS.todo.list });
-      const prevTodos = queryClient.getQueryData<TodoList>(
-        QUERY_KEYS.todo.list,
+      const prevTodo = queryClient.getQueryData<Todo>(
+        QUERY_KEYS.todo.detail(updatedTodo.id),
       );
-      queryClient.setQueryData<TodoList>(QUERY_KEYS.todo.list, (prevTodos) => {
-        if (!prevTodos) return [];
-        return prevTodos.map((prevTodo) => {
-          if (prevTodo.id !== updatedTodo.id) return prevTodo;
-          return {
-            ...prevTodo,
-            ...updatedTodo,
-          };
-        });
+
+      await queryClient.cancelQueries({
+        queryKey: QUERY_KEYS.todo.detail(updatedTodo.id),
       });
 
-      return { prevTodos };
+      queryClient.setQueryData<Todo>(
+        QUERY_KEYS.todo.detail(updatedTodo.id),
+        (prevTodo) => {
+          if (!prevTodo) return;
+          return { ...prevTodo, ...updatedTodo };
+        },
+      );
+
+      return {
+        prevTodo,
+      };
     },
     onError: (error, variable, context) => {
-      if (context && context.prevTodos) {
-        queryClient.setQueryData<TodoList>(
-          QUERY_KEYS.todo.list,
-          context.prevTodos,
+      if (context && context.prevTodo) {
+        queryClient.setQueryData<Todo>(
+          QUERY_KEYS.todo.detail(variable.id),
+          context.prevTodo,
         );
       }
-    },
-    onSettled: () => {
-      queryClient.invalidateQueries({
-        queryKey: QUERY_KEYS.todo.list,
-      });
     },
   });
 }
