03eea7d3eec4167c3cec3cf3605f170e11a7641d
y-minion <dudals2963@tukorea.ac.kr>
2025-10-30 15:12:56 +0900

    todo 캐시 데이터 평탄화

--- src/hooks/queries/use-todos-data.ts
+++ src/hooks/queries/use-todos-data.ts
@@ -1,11 +1,21 @@
 import { fetchTodos } from "@/api/fetch-todos";
 import { QUERY_KEYS } from "@/lib/constants";
-import { useQuery } from "@tanstack/react-query";
+import type { Todo } from "@/types";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 export function useTodosDate() {
+  const queryClient = useQueryClient();
   return useQuery({
     queryKey: QUERY_KEYS.todo.list,
-    queryFn: fetchTodos,
+    queryFn: async () => {
+      const todos = await fetchTodos();
+
+      todos.forEach((todo) => {
+        queryClient.setQueryData<Todo>(QUERY_KEYS.todo.detail(todo.id), todo);
+      });
+
+      return todos.map(({ id }) => id);
+    },
     retry: 0,
   });
 }
