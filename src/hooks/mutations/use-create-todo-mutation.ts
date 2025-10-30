98e1876c5b72345c2f34d93b0d33c84dab4eba77
y-minion <dudals2963@tukorea.ac.kr>
2025-10-30 17:27:16 +0900

    todo 생성 useCreateTodoMutation 훅 수정
    새로운 데이터 생성시 QUERY_KEYS.todo.list에 해당하는 쿼리 키에 캐시하도록 수정

--- src/hooks/mutations/use-create-todo-mutation.ts
+++ src/hooks/mutations/use-create-todo-mutation.ts
@@ -1,6 +1,6 @@
 import { createTodo } from "@/api/create-todo";
 import { QUERY_KEYS } from "@/lib/constants";
-import type { TodoList } from "@/types";
+import type { Todo, TodoList } from "@/types";
 import {
   QueryClient,
   useMutation,
@@ -18,11 +18,13 @@ export function useCreateTodoMutation() {
       window.alert(error.message);
     },
     onSuccess: (newTodo) => {
-      //   window.location.reload();
-      // queryClient.invalidateQueries({ queryKey: QUERY_KEYS.todo.list });
-      queryClient.setQueryData<TodoList>(QUERY_KEYS.todo.list, (prevTodos) => {
-        if (!prevTodos) return [newTodo];
-        return [...prevTodos, newTodo];
+      queryClient.setQueryData<Todo>(
+        QUERY_KEYS.todo.detail(newTodo.id),
+        newTodo,
+      );
+      queryClient.setQueryData<string[]>(QUERY_KEYS.todo.list, (prevData) => {
+        if (!prevData) return [newTodo.id];
+        return [...prevData, newTodo.id];
       });
     },
   });
